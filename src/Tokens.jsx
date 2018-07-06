793b8f386479bbba3ed5f2ab08b28540d8361b3f
Christopher Giroir <kelsin@valefor.com>
2018-07-06 12:25:14 -0700

    Better laid out tokens page

--- src/Tokens.jsx
+++ src/Tokens.jsx
@@ -8,18 +8,14 @@ const Tokens = ({ match }) => {
   let game = games[match.params.game];
   let companies = game.companies;
 
-  if(!companies) {
+  if (!companies) {
     return null;
   }
 
   let tokens = R.addIndex(R.chain)((company, index) => {
     let y = 60 * index + 30;
     let companyTokens = Array(company.tokens.length + 3).fill(
-      <Token
-        label={company.abbrev}
-        color={company.color}
-        width="30"
-      />
+      <Token label={company.abbrev} color={company.color} width="30" />
     );
     companyTokens.push(
       <Token
@@ -50,15 +46,21 @@ const Tokens = ({ match }) => {
   let y = 60 * companies.length + 30;
   tokens.push(<g transform={`translate(0 ${y})`}>{extras}</g>);
 
+  let width =
+    R.reduce(R.max, 1, R.map(c => c.tokens.length + 4, companies)) * 60;
+  let height = (companies.length + 1) * 60;
+
   return (
-    <svg width="800" height="1000">
-      <defs>
-        <style>
-          @import url('https://fonts.googleapis.com/css?family=Bitter:700');
-        </style>
-      </defs>
-      {tokens}
-    </svg>
+    <div className="tokens">
+      <svg width={width} height={height}>
+        <defs>
+          <style>
+            @import url('https://fonts.googleapis.com/css?family=Bitter:700');
+          </style>
+        </defs>
+        {tokens}
+      </svg>
+    </div>
   );
 };
 
