eb01ac927d2201b878fdd255182631d278761deb
Christopher Giroir <kelsin@valefor.com>
2018-07-06 22:12:29 -0700

    Adding bridge and tunnel atoms

---
+++ src/atoms/Bridge.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { colors } from "../data";
+
+const Bridge = ({ cost }) => {
+  return (
+    <g>
+      <path
+        d="M -28 -5 L 0 36 L 28 -5 Z"
+        fill={colors["water"]}
+        stroke={colors["white"]}
+        strokeWidth="2"
+        strokeLinecap="round"
+        x="0"
+        y="0"
+      />
+      <text
+        fill={colors["white"]}
+        fontSize="14"
+        alignmentBaseline="hanging"
+        textAnchor="middle"
+        x="0"
+        y="0"
+      >
+        {cost}
+      </text>
+    </g>
+  );
+};
+
+export default Bridge;
---
+++ src/atoms/Tunnel.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { colors } from "../data";
+
+const Tunnel = ({ cost }) => {
+  return (
+    <g>
+      <path
+        d="M -28 19 L 0 -22 L 28 19 Z"
+        fill={colors["mountain"]}
+        stroke={colors["white"]}
+        strokeWidth="2"
+        strokeLinecap="round"
+        x="0"
+        y="0"
+      />
+      <text
+        fill={colors["white"]}
+        fontSize="14"
+        alignmentBaseline="hanging"
+        textAnchor="middle"
+        x="0"
+        y="0"
+      >
+        {cost}
+      </text>
+    </g>
+  );
+};
+
+export default Tunnel;
