5468e601954fd29a289e7a8e327a2d0bba60c55b
Christopher Giroir <kelsin@valefor.com>
2018-07-18 14:15:16 -0700

    Simple icons and revenue page fixes, not complete

--- src/Players.jsx
+++ src/Players.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import * as R from "ramda";
 
-const Players = ({ players }) => {
+const Players = ({ players, bank, capital }) => {
   return (
     <div className="players">
+      {(bank || capital) && (
+        <table>
+          {bank && (
+            <tr>
+              <th>
+                <i className="fas fa-university" />
+              </th>
+              <td>{bank}</td>
+            </tr>
+          )}
+          {capital && (
+            <tr>
+              <th>
+                <i className="fas fa-coins" />
+              </th>
+              <td>{capital}</td>
+            </tr>
+          )}
+        </table>
+      )}
       <table>
         <thead>
           <tr>
-            <th>Players</th>
-            <th>Certificate Limit</th>
-            <th>Initial Capital</th>
+            <th>
+              <i className="fas fa-user-friends" />
+            </th>
+            <th>
+              <i className="fas fa-certificate" />
+            </th>
+            <th>
+              <i className="fas fa-coins" />
+            </th>
           </tr>
         </thead>
         <tbody>
