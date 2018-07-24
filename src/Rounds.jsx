70eac57ca03991de88ad9f80c1c704a177db8f81
Christopher Giroir <kelsin@valefor.com>
2018-07-24 15:52:10 -0700

    Adding arrows to rounds

--- src/Rounds.jsx
+++ src/Rounds.jsx
@@ -3,15 +3,27 @@ import Token from "./Token";
 import * as R from "ramda";
 
 const Rounds = ({ rounds }) => {
-  let items = R.map(
-    round => (
-      <svg viewBox="-25 -25 50 50" key={round.name}>
-        <Token width="25" label={round.name} color={round.color} labelColor="black" />
-      </svg>
+  let items = R.addIndex(R.map)(
+    (round, index) => (
+      <div className="Token">
+        <svg viewBox="-25 -25 50 50" key={round.name}>
+          <Token
+            width="25"
+            label={round.name}
+            color={round.color}
+            labelColor="black"
+          />
+        </svg>
+        {index > 0 && <i className="fas fa-arrow-up" />}
+      </div>
     ),
     rounds
   );
-  return <div className="rounds"><div className="tokens">{items}</div></div>;
+  return (
+    <div className="rounds">
+      <div className="tokens">{items}</div>
+    </div>
+  );
 };
 
 export default Rounds;
