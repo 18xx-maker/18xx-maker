8e97c3f673a99e984731a3b65e8d97c80071f299
Christopher Giroir <kelsin@valefor.com>
2018-12-02 22:31:52 -0800

    Adding fontSize to name

--- src/atoms/Name.jsx
+++ src/atoms/Name.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Name = ({ name, reverse, rotation, town, size, offset, y, textLength }) => {
+const Name = ({ name, reverse, rotation, town, size, offset, y, textLength, fontSize }) => {
   let path = town ? "townPath" : `city${size > 1 ? size : ""}Path`;
   if (reverse) {
     path = path + "Reverse";
@@ -10,7 +10,7 @@ const Name = ({ name, reverse, rotation, town, size, offset, y, textLength }) =>
       dy={y || (reverse ? (town ? 12 : 7) : (town ? -4 : 0))}
       transform={`rotate(${(rotation || 0) + 360})`}
       fontFamily="Helvetica, Arial, sans-serif"
-      fontSize="10"
+      fontSize={fontSize || 10}
       fontWeight="bold"
       textLength={textLength}
       textAnchor="middle"
