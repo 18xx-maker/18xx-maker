d7c856aaeee24ed77024b76f5b5b5ee7ae8154c9
Christopher Giroir <kelsin@valefor.com>
2019-01-06 20:51:56 -0800

    Offboard names default size = 11

--- src/atoms/Name.jsx
+++ src/atoms/Name.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { textColor } from "../data";
 
 const Name = ({ name, color, bgColor, path, rotation, reverse, offset, y, textLength, fontSize }) => {
-  let fillColor = color || textColor(bgColor || "white");
+  let fillColor = color || "black";
 
   let nameNode = path ? (
     <textPath transform="scale(1.8)" startOffset={`${offset || 50}%`} href={`#${path}`}>
@@ -16,7 +16,7 @@ const Name = ({ name, color, bgColor, path, rotation, reverse, offset, y, textLe
       transform={`rotate(${(rotation || 0) + 360})`}
       fill={fillColor}
       fontFamily="Helvetica, Arial, sans-serif"
-      fontSize={fontSize || 10}
+      fontSize={fontSize || 11}
       fontWeight="bold"
       textLength={textLength}
       alignmentBaseline={reverse ? "hanging" : "baseline"}
