a28a9c1f89bac8bd521c1b58619e97016a4b3b80
Christopher Giroir <kelsin@valefor.com>
2019-01-06 19:45:19 -0800

    Back to older service worker to unregister the right one

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from './serviceWorker';
+import { unregister } from "./registerServiceWorker";
 
 render(
   <Provider store={store}>
@@ -12,8 +12,4 @@ render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+unregister();
