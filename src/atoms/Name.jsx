436abaf4a5964f0d2f52739de522169b7151c258
Christopher Giroir <kelsin@valefor.com>
2019-01-23 15:37:09 -0800

    No more baseline manipulation on names

--- src/atoms/Name.jsx
+++ src/atoms/Name.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Name = ({ name, color, bgColor, path, rotation, reverse, offset, y, textLength, fontSize }) => {
   let fillColor = color || "black";
+  fontSize = fontSize || 11;
 
   let nameNode = path ? (
     <textPath startOffset={`${offset || 50}%`}
@@ -11,17 +12,21 @@ const Name = ({ name, color, bgColor, path, rotation, reverse, offset, y, textLe
     </textPath>
   ) : name;
 
+  y = y || 0;
+
+  if(!path && reverse) {
+    y += (0.75 * fontSize);
+  }
+
   return (
     <text
-      dy={y || 0}
+      dy={y}
       transform={`rotate(${(rotation || 0) + 360})`}
       fill={fillColor}
       fontFamily="Helvetica, Arial, sans-serif"
       fontSize={fontSize || 11}
       fontWeight="bold"
       textLength={textLength}
-      alignmentBaseline={reverse ? "hanging" : "baseline"}
-      dominantBaseline={path ? null : (reverse ? "hanging" : null)}
       textAnchor="middle"
     >
       {nameNode}
