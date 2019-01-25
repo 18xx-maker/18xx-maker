f7bb3a91e55851cbda8673d03c4dc248baa88a5b
Christopher Giroir <kelsin@valefor.com>
2019-01-25 15:36:28 -0800

    Printing script and 1860 updates

--- bin/print.js
+++ bin/print.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 const R = require('ramda');
+const express = require('express');
+const path = require('path');
+
+require("@babel/register");
 
 let game = process.argv[2] || "1830";
 
@@ -17,14 +21,12 @@ try {
   if (err.code !== 'EEXIST') throw err;
 }
 
-const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(process.cwd(), 'build')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(process.cwd(), 'build', 'index.html'));
 });
 
 const server = app.listen(9000);
@@ -33,19 +35,18 @@ const server = app.listen(9000);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // const items = ['background', 'cards', 'charters', 'map', 'map-paginated', 'tiles', 'tokens'];
-  const items = [];
+  const items = ['background', 'cards', 'charters', 'map', 'map-paginated', 'tiles', 'tokens'];
 
   for(let i=0;i<items.length;i++) {
     let item = items[i];
     console.log("Printing " + game + "/" + item);
-    await page.goto(`http://localhost:3000/${game}/${item}`, {waitUntil: 'networkidle2'});
+    await page.goto(`http://localhost:9000/${game}/${item}`, {waitUntil: 'networkidle2'});
     await page.pdf({path: `output/${game}/${item.replace(/\//g,'-')}.pdf`, scale: 1.0, preferCSSPageSize: true});
   }
 
   // Board 18 Output
-  let gameDef = require(`../src/data/games/${game}`);
-  let tileDefs = require('../src/data/tiles');
+  let gameDef = require(`../src/data/games/${game}`).default;
+  let tileDefs = require('../src/data/tiles').default;
   let counts = R.compose(
     R.countBy(R.identity),
     R.map(R.prop("color")),
@@ -60,10 +61,10 @@ const server = app.listen(9000);
     let height = 900;
 
     console.log("Printing " + game + "/b18/tiles/" + color);
-    await page.goto(`http://localhost:3000/${game}/b18/tiles/${color}`, {waitUntil: 'networkidle2'});
+    await page.goto(`http://localhost:9000/${game}/b18/tiles/${color}`, {waitUntil: 'networkidle2'});
     await page.pdf({path: `output/${game}/b18-tiles-${color}.pdf`, scale: 1.0, preferCSSPageSize: true});
     await page.setViewport({ width, height });
-    await page.addStyleTag({ content: '.App { padding: 0; } .GameMenu { display: none; }'});
+    await page.addStyleTag({ content: '.App {padding:0;} .Footer {display:none;} .GameMenu {display:none;}'});
     await page.screenshot({ path: `output/${game}/b18-tiles-${color}.png`, omitBackground: true });
   }
 
