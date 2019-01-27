49ca73e7e85bd02114252083b1691b318f0954a7
Christopher Giroir <kelsin@valefor.com>
2019-01-26 17:14:30 -0800

    Better notes on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,8 @@ import {NavLink} from "react-router-dom";
 
 import Tiles from "./nav/Tiles";
 
+import Color from "./data/Color";
+
 import assoc from "ramda/es/assoc";
 import compose from "ramda/es/compose";
 import map from "ramda/es/map";
@@ -28,25 +30,35 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h1>18xx <span>Print and Play Tool</span></h1>
-      <p>This site is a react client-side application which can take 18xx game
-        definitions written in json display them in a browser ready for
-        printing.</p>
-      <p>The original purpose was for personal pnp projects. Many games
-        displayed on this site are <strong>NOT</strong> licensed for public print and play.
-        A lot of great titles are availalbe from <a target="_blank" href="http://www.all-aboardgames.com/">All-Aboard Games</a> and <a target="_blank" href="http://www.goldenspikegames.com/">Golden Spike Games</a>.
-        If you are a designer and/or publisher and would like your design
-        to not be available either on this site or in the <a href="https://github.com/kelsin/18xx">source code</a> please <a href="mailto:kelsin@valefor.com">let me know</a>!</p>
-      <p><strong>Important:</strong> Please use this site to print copies of games for which you already
-        have a license by owning a version of the game. Information about games
-        which are available for free print and play can be found <a href="https://boardgamegeek.com/geeklist/58420/18xx-available-pnp">on this geeklist</a>.</p>
-      <p><strong>Note:</strong> Some games are still works in progress and in general the rendering of stock markets is not in a good place. Work will continue on this stuff.</p>
+      <Color>
+        {(c,t) => (
+          <React.Fragment>
+            <h1>18xx <span>Print and Play Tool</span></h1>
+            <p>This site is a react client-side application which can take 18xx game
+              definitions written in json display them in a browser ready for
+              printing.</p>
+            <p>The original purpose was for personal pnp projects. Many games
+              displayed on this site are <strong>NOT</strong> licensed for public print and play.
+              A lot of great titles are availalbe from <a target="_blank" href="http://www.all-aboardgames.com/">All-Aboard Games</a> and <a target="_blank" href="http://www.goldenspikegames.com/">Golden Spike Games</a>.
+              If you are a designer and/or publisher and would like your design
+              to not be available either on this site or in the <a href="https://github.com/kelsin/18xx">source code</a> please <a href="mailto:kelsin@valefor.com">let me know</a>!</p>
+            <p className="note" style={{backgroundColor:c("offboard"),color:t(c("offboard"))}}>
+              <strong>Important:</strong> Please use this site to print copies of games for which you already
+              have a license by owning a version of the game. Information about games
+              which are available for free print and play can be found <a href="https://boardgamegeek.com/geeklist/58420/18xx-available-pnp">on this geeklist</a>.
+            </p>
+            <p className="note" style={{backgroundColor:c("yellow"),color:t(c("yellow"))}}>
+              <strong>Note:</strong> Some games are still works in progress and in general the rendering of stock markets is not in a good place. Work will continue on this stuff.
+            </p>
+          </React.Fragment>
+        )}
+      </Color>
       <h2>Games</h2>
       <ul>
         {gameNodes}
       </ul>
       <h2>Tiles</h2>
-      <p>Tiles are defined separately from each game in (currently)
+      <p>Tiles are defined separately from each game in (currently){" "}
         <a href="https://github.com/kelsin/18xx/blob/master/src/data/tiles.js">one giant file</a>.
         Tiles are defined with the exactly the same format as map hexes. All available tile/hex elements are
         called atoms and examples of the json required for each is <NavLink to="/tiles/atoms">available</NavLink>.</p>
