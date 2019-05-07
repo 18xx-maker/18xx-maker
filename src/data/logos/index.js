364aa00ff8d729065b743784cbacadf59d2a9a8c
Christopher Giroir <kelsin@valefor.com>
2019-05-06 20:59:07 -0700

    Adding B&O and PRR Logo

--- src/data/logos/index.js
+++ src/data/logos/index.js
@@ -1,10 +1,12 @@
 import ATSF, { ReactComponent as ATSF_Component } from "./ATSF.svg";
+import BO, { ReactComponent as BO_Component } from "./BO.svg";
 import FW, { ReactComponent as FW_Component } from "./FW.svg";
 import GMO, { ReactComponent as GMO_Component } from "./GMO.svg";
 import IC, { ReactComponent as IC_Component } from "./IC.svg";
 import LIRR, { ReactComponent as LIRR_Component } from "./LIRR.svg";
 import MKT, { ReactComponent as MKT_Component } from "./MKT.svg";
 import MP, { ReactComponent as MP_Component } from "./MP.svg";
+import PRR, { ReactComponent as PRR_Component } from "./PRR.svg";
 import SLSF, { ReactComponent as SLSF_Component } from "./SLSF.svg";
 import SP, { ReactComponent as SP_Component } from "./SP.svg";
 import SSW, { ReactComponent as SSW_Component } from "./SSW.svg";
@@ -12,12 +14,14 @@ import TP, { ReactComponent as TP_Component } from "./TP.svg";
 
 export default {
   ATSF : { src: ATSF, Component: ATSF_Component },
+  "B&O" : { src: BO, Component: BO_Component },
   FW : { src: FW, Component: FW_Component },
   GMO : { src: GMO, Component: GMO_Component },
   IC : { src: IC, Component: IC_Component },
   LIRR : { src: LIRR, Component: LIRR_Component },
   MKT : { src: MKT, Component: MKT_Component },
   MP : { src: MP, Component: MP_Component },
+  PRR : { src: PRR, Component: PRR_Component },
   SLSF : { src: SLSF, Component: SLSF_Component },
   SP : { src: SP, Component: SP_Component },
   SSW : { src: SSW, Component: SSW_Component },
