5dd83406169450e95cd2f4ab87229f2942866274
Christopher Giroir <kelsin@valefor.com>
2019-05-06 17:19:26 -0700

    Adding LIRR logo

--- src/data/logos/index.js
+++ src/data/logos/index.js
@@ -2,6 +2,7 @@ import ATSF, { ReactComponent as ATSF_Component } from "./ATSF.svg";
 import FW, { ReactComponent as FW_Component } from "./FW.svg";
 import GMO, { ReactComponent as GMO_Component } from "./GMO.svg";
 import IC, { ReactComponent as IC_Component } from "./IC.svg";
+import LIRR, { ReactComponent as LIRR_Component } from "./LIRR.svg";
 import MKT, { ReactComponent as MKT_Component } from "./MKT.svg";
 import MP, { ReactComponent as MP_Component } from "./MP.svg";
 import SLSF, { ReactComponent as SLSF_Component } from "./SLSF.svg";
@@ -10,24 +11,15 @@ import SSW, { ReactComponent as SSW_Component } from "./SSW.svg";
 import TP, { ReactComponent as TP_Component } from "./TP.svg";
 
 export default {
-  ATSF : { src: ATSF,
-           Component: ATSF_Component },
-  FW : { src: FW,
-         Component: FW_Component },
-  GMO : { src: GMO,
-          Component: GMO_Component },
-  IC : { src: IC,
-         Component: IC_Component },
-  MKT : { src: MKT,
-          Component: MKT_Component },
-  MP : { src: MP,
-         Component: MP_Component },
-  SLSF : { src: SLSF,
-           Component: SLSF_Component },
-  SP : { src: SP,
-         Component: SP_Component },
-  SSW : { src: SSW,
-          Component: SSW_Component },
-  TP : { src: TP,
-         Component: TP_Component }
+  ATSF : { src: ATSF, Component: ATSF_Component },
+  FW : { src: FW, Component: FW_Component },
+  GMO : { src: GMO, Component: GMO_Component },
+  IC : { src: IC, Component: IC_Component },
+  LIRR : { src: LIRR, Component: LIRR_Component },
+  MKT : { src: MKT, Component: MKT_Component },
+  MP : { src: MP, Component: MP_Component },
+  SLSF : { src: SLSF, Component: SLSF_Component },
+  SP : { src: SP, Component: SP_Component },
+  SSW : { src: SSW, Component: SSW_Component },
+  TP : { src: TP, Component: TP_Component }
 };
