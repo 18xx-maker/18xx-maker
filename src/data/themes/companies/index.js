0559afe6eb7086c8328afa6a8a4aa6406216e555
Christopher Giroir <kelsin@valefor.com>
2019-05-11 23:21:44 -0700

    Moving to json theme files

---
+++ src/data/themes/companies/index.js
@@ -0,0 +1,15 @@
+import carth from "./carth";
+import dtg from "./dtg";
+import gmt from "./gmt";
+import ps18xx from "./ps18xx";
+import rob from "./rob";
+
+const companies = {
+  carth,
+  dtg,
+  gmt,
+  ps18xx,
+  rob
+};
+
+export default companies;
