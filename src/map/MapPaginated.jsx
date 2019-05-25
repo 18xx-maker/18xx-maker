45047f4b9dfd4b7fb4662c7cd6b018ad64339ba9
Christopher Giroir <kelsin@valefor.com>
2019-05-24 23:30:21 -0700

    Removing extra console.log

--- src/map/MapPaginated.jsx
+++ src/map/MapPaginated.jsx
@@ -46,8 +46,6 @@ const MapPaginated = ({ match, coords, pagination, paper, hexWidth }) => {
     pageHeight = tmp;
   }
 
-  console.log({pageWidth,pageHeight})
-
   let y = -25; // Start with room for margins
   let mapPages = map(height => {
     let x = -25; // Start with room for margins
