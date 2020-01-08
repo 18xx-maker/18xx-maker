c0bdf88b6d2667872891610c46ccf6048cf68c28
Christopher Giroir <kelsin@valefor.com>
2020-01-08 11:04:50 -0800

    Adding 1830 as an override and reordering 1857 so it works

--- src/data/companies/index.js
+++ src/data/companies/index.js
@@ -3,13 +3,15 @@ import languages from "./languages.json";
 import lotr from "./lotr.json";
 import owl from "./owl.json";
 import webdev from "./webdev.json";
+import game_1830 from "./1830.json";
 
 const companies = {
   dev,
   languages,
   lotr,
   owl,
-  webdev
+  webdev,
+  "1830": game_1830
 }
 
 export default companies;
