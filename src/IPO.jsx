a6668b80b6d87288528f63701b340ce92fd14a6f
Christopher Giroir <kelsin@valefor.com>
2020-01-13 09:35:23 -0800

    Working on IPO page slowly

--- src/IPO.jsx
+++ src/IPO.jsx
@@ -9,13 +9,13 @@ import PageSetup from "./PageSetup";
 
 import Par from "./Par";
 
-import { compileCompanies } from "./util";
+import { compileCompanies, unitsToCss } from "./util";
 
 import filter from "ramda/src/filter";
 import is from "ramda/src/is";
 import map from "ramda/src/map";
 
-require("./IPO.css");
+require("./IPO.scss");
 
 const IPO = () => {
   return (
@@ -29,18 +29,34 @@ const IPO = () => {
         }
 
         return (
-          <Color>
+          <Color context="companies">
             {c => (
-              <div className="ipo">
-                <h2>{game.info.title} IPO</h2>
+              <div className="ipo"
+                   style={{
+                     width: unitsToCss(config.paper.width),
+                     height: unitsToCss(config.paper.height),
+                     margin: unitsToCss(config.paper.margins),
+                   }}>
+                <h2>{game.info.title} Initial Public Offering</h2>
+                <Par par={game.stock.par} legend={game.stock.legend || []} />
                 <div className="ipo__companies">
                   {map(
                     company => (
-                      <div key={`ipo-${company.abbrev}`} className="ipo__company" style={{borderRadius: config.ipo.borderRadius, backgroundColor: c("plain")}}>
-                        <div className="ipo__token">
+                      <div key={`ipo-${company.abbrev}`}
+                           className="ipo__company"
+                           style={{
+                             borderRadius: unitsToCss(config.ipo.borderRadius),
+                             width: unitsToCss(config.cards.width),
+                             height: unitsToCss(config.cards.height),
+                             backgroundColor: company.color ? c(company.color) : c("plain")
+                           }}>
+                        <div className="ipo__token"
+                             style={{
+                               margin: `${unitsToCss((config.cards.height - 60) / 2)} auto`
+                             }}>
                           <ColorContext.Provider value="companies">
                             <Svg viewBox="-25 -25 50 50">
-                              <GameCompanyToken abbrev={company.abbrev} />
+                              <GameCompanyToken abbrev={company.abbrev} outline="white" />
                             </Svg>
                           </ColorContext.Provider>
                         </div>
@@ -49,8 +65,7 @@ const IPO = () => {
                     companies
                   )}
                 </div>
-                <Par par={game.stock.par} legend={game.stock.legend || []} />
-                <PageSetup landscape={true}/>
+                <PageSetup/>
               </div>
             )}
           </Color>
