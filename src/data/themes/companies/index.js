00a19b8d2501e7ad199d70fda4ba1beaf575a86f
broggles <59836835+broggles@users.noreply.github.com>
2020-01-14 09:53:53 +0000

    Add files via upload
    Broggles company theme based on 1817 redraw
    18TK them based on broggles redraw

--- src/data/themes/companies/index.js
+++ src/data/themes/companies/index.js
@@ -3,13 +3,17 @@ import dtg from "./dtg";
 import gmt from "./gmt";
 import ps18xx from "./ps18xx";
 import rob from "./rob";
+import broggles from "./broggles";
+import B18TK from "./B18TK";
 
 const companies = {
   carth,
   dtg,
   gmt,
   ps18xx,
-  rob
+  rob,
+  broggles,
+  B18TK
 };
 
 export default companies;
