c6be3d28b233c792943cedb3fa43775f87e5cfed
Christopher Giroir <kelsin@valefor.com>
2020-01-22 11:27:06 -0800

    Adding 18H8U market

--- src/data/games/index.js
+++ src/data/games/index.js
@@ -1,27 +1,23 @@
 import game_1800 from "./1800";
 import game_1817 from "./1817";
 import game_1825 from "./1825";
+import game_1825K1 from "./1825-K1";
+import game_1825K3 from "./1825-K3";
+import game_1825K3_1 from "./1825-R3.1";
+import game_1825K5 from "./1825-K5";
+import game_1825K6 from "./1825-K6";
+import game_1825Met from "./1825-Met";
+import game_1825R1_1 from "./1825-R1.1";
+import game_1825R2 from "./1825-R2";
 import game_1825U1 from "./1825-U1";
 import game_1825U2 from "./1825-U2";
 import game_1825U3 from "./1825-U3";
-import game_1825K1 from "./1825-K1";
-import game_1825K3 from "./1825-K3";
-import game_1825K5 from "./1825-K5";
-import game_1825K6 from "./1825-K6";
-import game_1825R1_1 from "./1825-R1.1";
-import game_1825R2 from "./1825-R2";
-import game_1825K3_1 from "./1825-R3.1";
-import game_1825Met from "./1825-Met";
-import game_1925DB1 from "./1825-DB1";
-import game_1925DB3 from "./1825-DB3";
-import game_1925DB5 from "./1825-DB5";
-import game_18Misc from "./18Misc";
 import game_1830 from "./1830";
 import game_1832 from "./1832";
 import game_1834 from "./1834";
+import game_1836 from "./1836";
 import game_1836jr_30 from "./1836jr-30";
 import game_1836jr_56 from "./1836jr-56";
-import game_1836 from "./1836";
 import game_1841 from "./1841";
 import game_1846 from "./1846";
 import game_1848 from "./1848";
@@ -41,17 +37,22 @@ import game_1891 from "./1891";
 import game_1899 from "./1899";
 import game_18AL from "./18AL";
 import game_18CLE from "./18CLE";
+import game_18Chesapeake from "./18Chesapeake";
 import game_18EB from "./18EB";
 import game_18EU from "./18EU";
+import game_18H8U from "./18H8U";
+import game_18Mex from "./18Mex";
+import game_18Misc from "./18Misc";
 import game_18NK from "./18NK";
 import game_18SS from "./18SS";
 import game_18SY from "./18SY";
-import game_18Chesapeake from "./18Chesapeake";
-import game_18Mex from "./18Mex";
 import game_18Scan from "./18Scan";
-import game_18Test from "./18Test";
 import game_18TE from "./18TE";
 import game_18TK from "./18TK";
+import game_18Test from "./18Test";
+import game_1925DB1 from "./1825-DB1";
+import game_1925DB3 from "./1825-DB3";
+import game_1925DB5 from "./1825-DB5";
 import game_FourthAge from "./FourthAge";
 import game_Harzbahn from "./Harzbahn";
 import game_Powerrails from "./Powerrails";
@@ -64,26 +65,26 @@ let all_games = {
   "1800": game_1800,
   "1817": game_1817,
   "1825": game_1825,
-  "1825-U1": game_1825U1,
-  "1825-U2": game_1825U2,
-  "1825-U3": game_1825U3,
+  "1825-DB1": game_1925DB1,
+  "1825-DB3": game_1925DB3,
+  "1825-DB5": game_1925DB5,
   "1825-K1": game_1825K1,
   "1825-K3": game_1825K3,
   "1825-K5": game_1825K5,
   "1825-K6": game_1825K6,
+  "1825-Met": game_1825Met,
   "1825-R1.1": game_1825R1_1,
   "1825-R2": game_1825R2,
   "1825-R3.1": game_1825K3_1,
-  "1825-Met": game_1825Met,
-  "1825-DB1": game_1925DB1,
-  "1825-DB3": game_1925DB3,
-  "1825-DB5": game_1925DB5,
+  "1825-U1": game_1825U1,
+  "1825-U2": game_1825U2,
+  "1825-U3": game_1825U3,
   "1830": game_1830,
   "1832": game_1832,
   "1834": game_1834,
+  "1836": game_1836,
   "1836jr-30": game_1836jr_30,
   "1836jr-56": game_1836jr_56,
-  "1836": game_1836,
   "1841": game_1841,
   "1846": game_1846,
   "1848": game_1848,
@@ -103,14 +104,15 @@ let all_games = {
   "1899": game_1899,
   "18AL": game_18AL,
   "18CLE": game_18CLE,
+  "18Chesapeake": game_18Chesapeake,
   "18EB": game_18EB,
   "18EU": game_18EU,
+  "18H8U": game_18H8U,
+  "18Mex": game_18Mex,
   "18Misc": game_18Misc,
   "18NK": game_18NK,
   "18SS": game_18SS,
   "18SY": game_18SY,
-  "18Chesapeake": game_18Chesapeake,
-  "18Mex": game_18Mex,
   "18Scan": game_18Scan,
   "18TE": game_18TE,
   "18TK": game_18TK,
