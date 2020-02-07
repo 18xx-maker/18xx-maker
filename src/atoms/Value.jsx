7fa9c65f5652037bb3048df1b7ea0b7ef6175895
Christopher Giroir <kelsin@valefor.com>
2020-02-07 13:32:13 -0800

    Terrain fix and square values

--- src/atoms/Value.jsx
+++ src/atoms/Value.jsx
@@ -5,7 +5,7 @@ import Config from "../data/Config";
 import Currency, { format } from "../util/Currency";
 import RotateContext from "../context/RotateContext";
 
-const Value = ({ value, fixed, outerBorderColor }) => {
+const Value = ({ value, shape, fixed, outerBorderColor }) => {
   return (
     <RotateContext.Consumer>
       {rotation => (
@@ -15,46 +15,66 @@ const Value = ({ value, fixed, outerBorderColor }) => {
             let size = 15;
             let ry = 14;
             let rx = length > 2 ? length * 6 : 14;
+
             return (
               <Color>
-                {(c,t,s,p) => (
-                  <g>
-                    if(outerBorderColor) {
-                      <ellipse
-                        stroke={p(outerBorderColor)}
-                        strokeWidth="7"
-                        cx="0"
-                        cy="0"
-                        rx={rx}
-                        ry={ry}
-                      />
+                {(c,t,s,p) => {
+
+                  let outline = null;
+                  let bg = null
+                  if (shape === "square") {
+                    if (outerBorderColor) {
+                      outline = (
+                        <rect stroke={p(outerBorderColor)} strokeWidth="7"
+                              x={-rx} y={-ry}
+                              width={2*rx} height={2*ry} />
+                      );
                     }
-                    <ellipse
-                      fill={p("white")}
-                      stroke={p("black")}
-                      strokeWidth="2"
-                      cx="0"
-                      cy="0"
-                      rx={rx}
-                      ry={ry}
-                    />
-                    <text
-                      transform={fixed ? null : `rotate(-${rotation})`}
-                      fontWeight="bold"
-                      fontSize={size}
-                      fontFamily="sans-serif"
-                      fill={p("black")}
-                      dominantBaseline="central"
-                      textAnchor="middle"
-                      textLength={length > 2 ? (rx + 6) : null}
-                      lengthAdjust="spacingAndGlyphs"
-                      x="0"
-                      y="0"
-                    >
-                      <Currency value={value} type="value"/>
-                    </text>
-                  </g>
-                )}
+
+                    bg = (
+                      <rect fill={p("white")} stroke={p("black")} strokeWidth="2"
+                            x={-rx} y={-ry}
+                            width={2*rx} height={2*ry} />
+                    );
+                  } else {
+                    if (outerBorderColor) {
+                      outline = (
+                        <ellipse stroke={p(outerBorderColor)} strokeWidth="7"
+                                 cx="0" cy="0"
+                                 rx={rx} ry={ry} />
+                      );
+                    }
+
+                    bg = (
+                      <ellipse fill={p("white")} stroke={p("black")} strokeWidth="2"
+                               cx="0" cy="0"
+                               rx={rx} ry={ry}
+                      />
+                    );
+                  }
+
+                  return (
+                    <g>
+                      {outline}
+                      {bg}
+                      <text
+                        transform={fixed ? null : `rotate(-${rotation})`}
+                        fontWeight="bold"
+                        fontSize={size}
+                        fontFamily="sans-serif"
+                        fill={p("black")}
+                        dominantBaseline="central"
+                        textAnchor="middle"
+                        textLength={length > 2 ? (rx + 6) : null}
+                        lengthAdjust="spacingAndGlyphs"
+                        x="0"
+                        y="0"
+                      >
+                        <Currency value={value} type="value"/>
+                      </text>
+                    </g>
+                  );
+                }}
               </Color>
             );
           }}
