aa3fa767b02e0b548bb20daf3eabb4fcb2b1cd0c
Christopher Giroir <kelsin@valefor.com>
2020-05-17 16:16:24 -0700

    fix: Update all dependencies and fix validation errors

--- src/Phase.jsx
+++ src/Phase.jsx
@@ -49,19 +49,40 @@ const matchTrain = R.curry((phase, train) => {
     return false;
   }
 
-  if (phase.name === train.name) {
-    return true;
+  if (phase.train) {
+    if (phase.train === train.name) {
+      return true;
+    }
+
+    if (Array.isArray(phase.train) && phase.train.includes(train.name)) {
+      return true;
+    }
+
+    return false;
   }
 
-  if (phase.train === train.name) {
-    return true;
+  return phase.name === train.name;
+});
+
+const matchTrainEvent = R.curry((eventTrain, event, train) => {
+  if (train.phase === false) {
+    return false;
   }
 
-  if (Array.isArray(phase.train) && phase.train.includes(train.name)) {
-    return true;
-  }
+  let trainEvent = train[event];
+  let events = R.is(Array, trainEvent) ? [...trainEvent] : [...[trainEvent]];
 
-  return false;
+  return R.any(e => {
+    if (R.is(Object, e)) {
+      return e.on === eventTrain.name;
+    }
+
+    return e === eventTrain.name;
+  }, events);
+});
+
+const matchEvent = R.curry((phaseTrains, event, train) => {
+  return R.any(pt => matchTrainEvent(pt, event, train), phaseTrains);
 });
 
 const Phase = ({ phases, trains, minor, company }) => {
@@ -75,7 +96,7 @@ const Phase = ({ phases, trains, minor, company }) => {
 
   let phaseRows = R.compose(
     R.map(phase => {
-      let notes = phase.notes ? (Array.isArray(phase.notes) ? phase.notes : [phase.notes]) : [];
+      let notes = phase.notes ? (Array.isArray(phase.notes) ? [...phase.notes] : [phase.notes]) : [];
       if (phase.buy_companies) {
         notes.push("Private companies may be purchased.")
       }
@@ -85,13 +106,14 @@ const Phase = ({ phases, trains, minor, company }) => {
       if ((phase.events || {}).remove_tokens) {
         notes.push("Private tokens removed.")
       }
-      notes = R.intersperse(<br/>, notes);
+      let noteNodes = R.addIndex(R.map)((n, i) => <li key={i}>{n}</li>, notes);
 
       // Find all trains linked to this phase
       let phaseTrains = R.filter(matchTrain(phase), trains || []);
 
       // Get the names for each train
-      let trainNames = R.map(t => <li key={t.name}>{t.name}</li>, phaseTrains);
+      let trainNames = R.map(R.prop("name"), phaseTrains);
+      let trainNodes = R.map(name => <li key={name}>{name}</li>, trainNames);
 
       // Prices for each trach
       let prices = R.map(t => <li key={t.name}>{formatCell(t.price)}</li>, phaseTrains);
@@ -100,19 +122,19 @@ const Phase = ({ phases, trains, minor, company }) => {
       let quantities = R.map(t => <li key={t.name}>{t.quantity}</li>, phaseTrains);
 
       // Get all trains that rust on this phase
-      let rustingTrains = R.filter(t => t.phase !== false && t.rust === phase.name, trains || []);
+      let rustingTrains = R.filter(matchEvent(phaseTrains, "rust"), trains || []);
 
       // Which trains rust during this phase
       let rusts = R.map(t => <li key={t.name}>{t.name}</li>, rustingTrains);
 
       // Get all trains that rust on this phase
-      let obsoleteTrains = R.filter(t => t.phase !== false && t.obsolete === phase.name, trains || []);
+      let obsoleteTrains = R.filter(matchEvent(phaseTrains, "obsolete"), trains || []);
 
       // Which trains rust during this phase
       let obsoletes = R.map(t => <li key={t.name}>{t.name}</li>, obsoleteTrains);
 
       // Get all trains that rust on this phase
-      let phasedTrains = R.filter(t => t.phase !== false && t.phased === phase.name, trains || []);
+      let phasedTrains = R.filter(matchEvent(phaseTrains, "phased"), trains || []);
 
       // Which trains rust during this phase
       let phased = R.map(t => <li key={t.name}>{t.name}</li>, phasedTrains);
@@ -123,7 +145,7 @@ const Phase = ({ phases, trains, minor, company }) => {
             <tr key={phase.name}>
               {includeName && <td>{phase.name}</td>}
               {includePhase && <td>{phase.phase}</td>}
-              {includeTrain && <td><ul>{trainNames}</ul></td>}
+              {includeTrain && <td><ul>{trainNodes}</ul></td>}
               <td><ul>{prices}</ul></td>
               <td><ul>{quantities}</ul></td>
               <td>{phase.limit}</td>
@@ -131,7 +153,7 @@ const Phase = ({ phases, trains, minor, company }) => {
               {includeObsolete && <td><ul>{obsoletes}</ul></td>}
               {includeRust && <td><ul>{rusts}</ul></td>}
               {includeTiles && <td style={{ backgroundColor: c(phase.tiles) }}>&nbsp;</td>}
-              <td className="phase__notes">{notes}</td>
+              <td className="phase__notes"><ul>{noteNodes}</ul></td>
             </tr>
           )}
         </Color>
