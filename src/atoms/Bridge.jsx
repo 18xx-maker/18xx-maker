04947c9351c49e0710435d49036cc5cb694eaa54
th3tick <48138042+th3tick@users.noreply.github.com>
2020-05-23 23:44:35 -0600

    adding circles, and passing the strokeDashArray and fillOpacity to the others (#307)
    Co-authored-by: Christopher Giroir <kelsin@valefor.com>

--- src/atoms/Bridge.jsx
+++ src/atoms/Bridge.jsx
@@ -3,13 +3,15 @@ import Color from "../data/Color";
 import PhaseContext from "../context/PhaseContext";
 import Currency from "../util/Currency";
 
-const Bridge = ({ cost, fillColor, strokeColor, strokeWidth, textColor, fontFamily, fontSize, fontWeight, width }) => {
+const Bridge = ({ cost, fillColor, fillOpacity, strokeColor, strokeWidth, textColor, fontFamily, fontSize, fontWeight, width, strokeDashArray }) => {
   fillColor = fillColor || "water";
   strokeColor = strokeColor || "track";
   strokeWidth = strokeWidth >= 0 ? strokeWidth : "1";
   fontFamily = fontFamily || "display";
   fontSize = fontSize || "12";
   fontWeight = fontWeight || "bold";
+  strokeDashArray = strokeDashArray || "";
+  fillOpacity = fillOpacity || "1";
   let scale = width !== 0 ? width / 22 : 22;
   return (
     <PhaseContext.Provider value="default">
@@ -22,6 +24,8 @@ const Bridge = ({ cost, fillColor, strokeColor, strokeWidth, textColor, fontFami
                 fill={c(fillColor)}
                 stroke={c(strokeColor)}
                 strokeWidth={strokeWidth}
+                fill-opacity={fillOpacity}
+                stroke-dasharray={strokeDashArray}
                 strokeLinecap="round"
                 x="0"
                 y="0"
--- src/atoms/Hexagon.jsx
+++ src/atoms/Hexagon.jsx
@@ -3,13 +3,15 @@ import Color from "../data/Color";
 import PhaseContext from "../context/PhaseContext";
 import Currency from "../util/Currency";
 
-const Diamond = ({ cost, fillColor, strokeColor, strokeWidth, textColor, fontFamily, fontSize, fontWeight, width }) => {
+const Hexagon = ({ cost, fillColor, fillOpacity, strokeColor, strokeWidth, textColor, fontFamily, fontSize, fontWeight, width, strokeDashArray }) => {
   fillColor = fillColor || "mountain";
   strokeColor = strokeColor || "black";
   strokeWidth = strokeWidth >= 0 ? strokeWidth : "1";
   fontFamily = fontFamily || "display";
   fontSize = fontSize || "12";
   fontWeight = fontWeight || "bold";
+  strokeDashArray = strokeDashArray || "";
+  fillOpacity = fillOpacity || "1";
   let scale = width !== 0 ? width / 22 : 22;
   let texty = -5 - (fontSize-12)/6;
   return (
@@ -23,6 +25,8 @@ const Diamond = ({ cost, fillColor, strokeColor, strokeWidth, textColor, fontFam
                 fill={c(fillColor)}
                 stroke={c(strokeColor)}
                 strokeWidth={strokeWidth}
+                fill-opacity={fillOpacity}
+                stroke-dasharray={strokeDashArray}
                 strokeLinecap="round"
                 x="0"
                 y="0"
@@ -47,4 +51,4 @@ const Diamond = ({ cost, fillColor, strokeColor, strokeWidth, textColor, fontFam
   );
 };
 
-export default Diamond;
+export default Hexagon;
