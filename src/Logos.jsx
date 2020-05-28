733672929cd2cc6fa076e015d01abfbb73a947eb
Christopher Giroir <kelsin@valefor.com>
2020-05-27 22:27:54 -0700

    fix: app icons

--- src/Logos.jsx
+++ src/Logos.jsx
@@ -2,24 +2,20 @@ import React from "react";
 
 import logos from "./data/logos";
 
-import chain from "ramda/src/chain";
+import map from "ramda/src/map";
 import toPairs from "ramda/src/toPairs";
 
 import "./Logos.scss";
 
-const logoNodes = chain(([label, company]) => {
-  if (company.src) {
-    return [(
-      <div key={label}>
-        <h2>{label}</h2>
-        <a href={encodeURIComponent(company.src)}>
-          <company.Component width="128" height="128"/>
-        </a>
-      </div>
-    )];
-  } else {
-    return logoNodes(toPairs(company))
-  }
+const logoNodes = map(([label, company]) => {
+  return (
+    <div key={label}>
+      <h2>{label}</h2>
+      <a href={encodeURIComponent(company.src)}>
+        <company.Component width="128" height="128"/>
+      </a>
+    </div>
+  );
 });
 
 const Logos = () => {
