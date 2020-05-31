89424b0854ee7ece2d52c84686c45b3564d7823b
Christopher Giroir <kelsin@valefor.com>
2020-05-31 14:26:12 -0700

    feat: sidenav

--- src/nav/SideNav.jsx
+++ src/nav/SideNav.jsx
@@ -5,43 +5,59 @@ import Drawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
 import Toolbar from '@material-ui/core/Toolbar';
 
+import IfSideMenu from "./IfSideMenu";
+
 import { makeStyles } from '@material-ui/core/styles';
+
+import DocsNav from "./DocsNav";
+import ElementsNav from "./ElementsNav";
 import GameNav from "./GameNav";
 
 const useStyles = makeStyles((theme) => ({
   sideNav: {
-    width: 200
+    width: 300
   }
 }));
 
 const SideNav = ({open, toggle}) => {
   const classes = useStyles();
 
-  return (
+  const menu = (
     <>
+      <Toolbar/>
+      <Route path="/games">
+        <GameNav/>
+      </Route>
+      <Route path="/elements">
+        <ElementsNav/>
+      </Route>
+      <Route path="/docs">
+        <DocsNav/>
+      </Route>
+    </>
+  );
+
+  return (
+    <IfSideMenu>
       <Hidden mdUp>
         <Drawer variant="temporary"
                 open={open}
                 onClose={toggle}
                 anchor="left"
+                style={{zIndex:1200}}
+                transitionDuration={200}
                 ModalProps={{keepMounted: true}}
                 PaperProps={{className: classes.sideNav}}>
-          <Toolbar/>
-          <Route path="/games/:slug">
-            <GameNav/>
-          </Route>
+          {menu}
         </Drawer>
       </Hidden>
       <Hidden smDown>
         <Drawer variant="permanent"
                 PaperProps={{className: classes.sideNav}}>
-          <Toolbar/>
-          <Route path="/games/:slug">
-            <GameNav/>
-          </Route>
+          {menu}
         </Drawer>
       </Hidden>
-    </>
+    </IfSideMenu>
   );
 }
 
--- src/util/File.jsx
+++ src/util/File.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+
 import Button from "@material-ui/core/Button";
 import DownloadIcon from '@material-ui/icons/GetApp';
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
 import is from "ramda/src/is";
+import { isElectron } from "../util";
 
 const File = (props) => {
   const mime = props.mime || "application/json";
+  const verb = isElectron ? "Save" : "Download";
 
   let data = props.data;
   if (is(Object, data)) {
@@ -18,12 +24,26 @@ const File = (props) => {
     data = data.toString('base64');
   }
 
+  if (props.list) {
+    return <ListItem button
+                     component="a"
+                     download={props.filename}
+                     href={`data:${mime};base64,${data}`}>
+             <ListItemIcon>
+               <DownloadIcon/>
+             </ListItemIcon>
+             <ListItemText primary={verb}
+                           secondary={props.filename}/>
+           </ListItem>;
+  }
+
   return <Button download={props.filename}
                  startIcon={<DownloadIcon/>}
                  variant="contained"
                  color="primary"
-                 href={`data:${mime};base64,${data}`}
-                 {...props}/>
+                 href={`data:${mime};base64,${data}`}>
+           {verb} {props.filename}
+         </Button>
 };
 
 export default File;
