ba9c495758ad225cd369b6bd41306336bd8f2700
Christopher Giroir <kelsin@valefor.com>
2020-06-03 11:59:24 -0700

    fix: tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './i18n';
 import App from './App';
-
-import { Provider } from "react-redux";
-import store from "./store";
+import { StaticRouter } from "react-router";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+  ReactDOM.render(<StaticRouter location="/"><App/></StaticRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
