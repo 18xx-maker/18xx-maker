43cdfee2048b1f96119c8ec87e5eb92fde9e5d20
Christopher Giroir <kelsin@valefor.com>
2020-06-15 20:27:27 -0700

    feat: notarizing

---
+++ electron/notarize.js
@@ -0,0 +1,17 @@
+const { notarize } = require("electron-notarize");
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: "com.18xx-maker.app",
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: "kelsin@valefor.com",
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
