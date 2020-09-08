b795963caa6f44e94cbbae8b2e732578823413b5
John Harres <jmharres@gmail.com>
2020-09-08 08:01:38 -0600

    adding ability to choose which axis has letters or numbers

--- src/map/Coordinates.jsx
+++ src/map/Coordinates.jsx
@@ -26,8 +26,28 @@ export const Coordinate = ({x, y, label}) => (
   </Color>
 );
 
-export const Coordinates = ({horizontal, coords, maxX, maxY, hexX, hexY, totalWidth, totalHeight, topCoord, bottomCoord, leftCoord, rightCoord}) => {
+export const Coordinates = ({horizontal, coords, mapCoordinates, maxX, maxY, hexX, hexY, totalWidth, totalHeight, topCoord, bottomCoord, leftCoord, rightCoord}) => {
   if (coords !== "edge" && coords !== "outside") {
+    return null;
+  }
+
+  // By default, numbers go across the top/bottom, and letters down the sides.
+  // "horizontal" maps have letters across the top/bottom by default and 
+  //   numbers down the sides..
+  // horizontal is set to true or false at this point
+  var flipped;
+  switch(mapCoordinates) {
+  case "reversed":
+  case "numbersHorizontal":
+  case "lettersVertical":
+    flipped = !horizontal;
+    break;
+  case "numbersVertical":
+  case "lettersHorizontal":
+  case undefined:
+    flipped = horizontal;
+    break;
+  default:
     return null;
   }
 
@@ -35,20 +55,20 @@ export const Coordinates = ({horizontal, coords, maxX, maxY, hexX, hexY, totalWi
     <Coordinate key={`start-row-${x}`}
                 x={horizontal ? hexX(0, x) : hexX(x, 0)}
                 y={coords === "edge" ? topCoord(x) : 25}
-                label={horizontal ? toAlpha(x) : x} />,
+                label={flipped ? toAlpha(x) : x} />,
     <Coordinate key={`end-row-${x}`}
                 x={horizontal ? hexX(0, x) : hexX(x, 0)}
                 y={coords === "edge" ? bottomCoord(x) : (totalHeight - 25)}
-                label={horizontal ? toAlpha(x) : x} />
+                label={flipped ? toAlpha(x) : x} />
   ], range(1, maxX + 1)), chain(y => [
     <Coordinate key={`start-col-${y}`}
                 x={coords === "edge" ? leftCoord(y) : 25}
                 y={horizontal ? hexY(y, 0) : hexY(0, y)}
-                label={horizontal ? y : toAlpha(y)} />,
+                label={flipped ? y : toAlpha(y)} />,
     <Coordinate key={`end-col-${y}`}
                 x={coords === "edge" ? rightCoord(y) : (totalWidth - 25)}
                 y={horizontal ? hexY(y, 0) : hexY(0, y)}
-                label={horizontal ? y : toAlpha(y)} />
+                label={flipped ? y : toAlpha(y)} />
   ], range(1, maxY + 1)));
 };
 
