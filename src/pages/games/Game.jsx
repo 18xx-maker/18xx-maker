abdd9a6de01f6bb060699818050de93eafdb932c
Christopher Giroir <kelsin@valefor.com>
2020-09-11 11:59:37 -0700

    feat: added links to game page and game nav

--- src/pages/games/Game.jsx
+++ src/pages/games/Game.jsx
@@ -8,6 +8,7 @@ import Card from "./Card";
 import Cards from "./Cards";
 import Charter from "./Charter";
 import Charters from "./Charters";
+import Link from "@material-ui/core/Link";
 import Map from "./Map";
 import Market from "./Market";
 import Par from "./Par";
@@ -32,6 +33,12 @@ import Typography from "@material-ui/core/Typography";
 
 import PlayersIcon from "@material-ui/icons/People";
 
+import BGGIcon from "@material-ui/icons/Storage";
+import LicenseIcon from "@material-ui/icons/Lock";
+import PurchaseIcon from "@material-ui/icons/MonetizationOn";
+import RulesIcon from "@material-ui/icons/Gavel";
+import WarningIcon from "@material-ui/icons/Warning";
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -39,6 +46,9 @@ const useStyles = makeStyles((theme) => ({
     overflow: 'auto',
     margin: theme.spacing(4, 0),
     padding: theme.spacing(2, 2, 0, 2)
+  },
+  warning: {
+    color: theme.palette.warning.main
   }
 }));
 
@@ -123,6 +133,58 @@ const Game = ({game}) => {
                                    <ListItemText primary={`${game.players[0].number} - ${game.players[game.players.length - 1].number}`}
                                                  secondary={t('game.players')}/>
                                  </ListItem>}
+                {game.links && game.links.license && (
+                  <ListItem button
+                            component={Link}
+                            color="inherit"
+                            underline="none"
+                            target="_blank"
+                            href={game.links.license}>
+                    <ListItemIcon><LicenseIcon color="error" /></ListItemIcon>
+                    <ListItemText primary={t('game.license.primary')} secondary={t('game.license.secondary')} />
+                  </ListItem>
+                )}
+                {game.links && game.links.purchase && (
+                  <ListItem button
+                            component={Link}
+                            color="inherit"
+                            underline="none"
+                            target="_blank"
+                            href={game.links.purchase}>
+                    <ListItemIcon><PurchaseIcon /></ListItemIcon>
+                    <ListItemText primary={t('game.purchase.primary')} secondary={t('game.purchase.secondary')} />
+                  </ListItem>
+                )}
+                {game.links && game.links.bgg && (
+                  <ListItem button
+                            component={Link}
+                            color="inherit"
+                            underline="none"
+                            target="_blank"
+                            href={game.links.bgg}>
+                    <ListItemIcon><BGGIcon/></ListItemIcon>
+                    <ListItemText>{t('game.bgg')}</ListItemText>
+                  </ListItem>
+                )}
+                {game.links && game.links.rules && (
+                  <ListItem button
+                            component={Link}
+                            color="inherit"
+                            underline="none"
+                            target="_blank"
+                            href={game.links.rules}>
+                    <ListItemIcon><RulesIcon/></ListItemIcon>
+                    <ListItemText primary={t('game.rules')} />
+                  </ListItem>
+                )}
+                {game.wip && (
+                  <ListItem>
+                    <ListItemIcon>
+                      <WarningIcon className={classes.warning}/>
+                    </ListItemIcon>
+                    <ListItemText primary={t('wip.wip')} secondary={t('wip.description')}/>
+                  </ListItem>
+                )}
               </List>
             </Paper>
           </Container>
