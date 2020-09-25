85c8eb6ca4c3c74b7f9f9fd2276de62a1491f0d2
John Harres <jmharres@gmail.com>
2020-09-24 19:56:13 -0600

    shrinking for multiple W, M, and Ns in an abbreviation

--- src/tokens/Token.jsx
+++ src/tokens/Token.jsx
@@ -474,15 +474,12 @@ const Token = ({
               fSize *= numbersOnlyScaling;
             }
           }
-          if (label.indexOf("W") !== -1) {
-            fSize *= 0.85;
-          }
-          if (label.indexOf("M") !== -1) {
-            fSize *= 0.9;
-          }
-          if (label.indexOf("N") !== -1) {
-            fSize *= 0.95;
-          }
+
+          /* W, M, and N are wider than average, so shrink to accomodate */
+          fSize *= 0.85 ** (label.match(/W/g)||[]).length;
+          fSize *= 0.9 ** (label.match(/M/g)||[]).length;
+          fSize *= 0.95 ** (label.match(/N/g)||[]).length;
+
           let y = fSize * 11 / 32;
           if (shield || shield3) {
             fSize *= scaling;
