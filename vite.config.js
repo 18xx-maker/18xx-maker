457bf1efe4f49af0133a4ca6447636347ed001c6
Christopher Giroir <kelsin@valefor.com>
2024-11-04 17:16:26 -0800

    feat: adding vite and other plugins

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  // depending on your application, base can also be "/"
+  base: "",
+  plugins: [react(), svgr()],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000,
+  },
+});
