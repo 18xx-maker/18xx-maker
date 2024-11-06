8c855a0a37edd2effce85a8f46e3c79872b8d9bc
Christopher Giroir <kelsin@valefor.com>
2024-11-05 18:23:04 -0800

    fix: clean up deps

--- src/data/games/index.js
+++ src/data/games/index.js
@@ -5,7 +5,7 @@ let gameImports = import.meta.glob("./*.json", { eager: true, import: "default" 
 let games = {};
 
 for (let [filename, game] of Object.entries(gameImports)) {
-  let id = replace(/\.json$/, "", filename);
+  let id = replace(/^\.\/(.*)\.json$/, "$1", filename);
   let slug = encodeURIComponent(id);
   let minPlayers = game.players ? game.players[0].number : 0;
   let maxPlayers = game.players ? game.players[game.players.length - 1].number : 0;
