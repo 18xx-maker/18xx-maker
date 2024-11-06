7680aefc038527bef789782dda1d21985c641e64
Christopher Giroir <kelsin@valefor.com>
2024-11-05 18:56:37 -0800

    feat: react upgrade to 16.14.0 (#502)
    * fix: speed up github workflow
    
    * fix: clean up deps
    
    * chore: move to vitest

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import browserslistToEsbuild from 'browserslist-to-esbuild'
 
@@ -17,9 +17,7 @@ export default defineConfig({
     }
   },
   plugins: [
-    react({
-      jsxRuntime: "classic",
-    }),
+    react(),
     svgr(),
   ],
   assetsInclude: ["**/*.md"],
