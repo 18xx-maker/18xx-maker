a9ed80f67688c48cc6aca7a0bc753e27906b8379
Christopher Giroir <kelsin@valefor.com>
2024-11-05 20:10:45 -0800

    fix: remove one use of Suspense

--- src/pages/elements/Logos.jsx
+++ src/pages/elements/Logos.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 
 import { useTranslation } from "react-i18next";
 
--- src/pages/games/Game.jsx
+++ src/pages/games/Game.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Redirect, Route, Switch } from "react-router";
 import { matchPath, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -69,133 +69,131 @@ const Game = ({game}) => {
   }
 
   return (
-    <Suspense fallback={null}>
-      <Switch>
-        <Route path="/games/:slug/b18/map">
-          <B18Map/>
-        </Route>
-        <Route path="/games/:slug/b18/tiles/:color*">
-          <B18Tiles/>
-        </Route>
-        <Route path="/games/:slug/b18/tokens">
-          <B18Tokens/>
-        </Route>
-        <Route path="/games/:slug/background">
-          <Background/>
-        </Route>
-        <Route path="/games/:slug/cards/:type/:index">
-          <Card/>
-        </Route>
-        <Route path="/games/:slug/cards">
-          <Cards/>
-        </Route>
-        <Route path="/games/:slug/charters/:index">
-          <Charter/>
-        </Route>
-        <Route path="/games/:slug/charters">
-          <Charters/>
-        </Route>
-        <Route path="/games/:slug/map">
-          <Map/>
-        </Route>
-        <Route path="/games/:slug/market">
-          <Market/>
-        </Route>
-        <Route path="/games/:slug/par">
-          <Par/>
-        </Route>
-        <Route path="/games/:slug/revenue">
-          <Revenue/>
-        </Route>
-        <Route path="/games/:slug/tile-manifest">
-          <TileManifest/>
-        </Route>
-        <Route path="/games/:slug/tiles/:id">
-          <Tile/>
-        </Route>
-        <Route path="/games/:slug/tiles">
-          <Tiles/>
-        </Route>
-        <Route path="/games/:slug/tokens/:index">
-          <Token/>
-        </Route>
-        <Route path="/games/:slug/tokens">
-          <Tokens/>
-        </Route>
-        <Route>
-          <Container maxWidth="md">
-            <Paper elevation={5} className={classes.page}>
-              <Typography variant="h3">{game.info.title}</Typography>
-              {game.info.subtitle && <Typography variant="h5">{game.info.subtitle}</Typography>}
-              <Typography variant="h6">{t('game.by')} {game.info.designer}</Typography>
-              <List>
-                {game.players && <ListItem>
-                                   <ListItemIcon><PlayersIcon/></ListItemIcon>
-                                   <ListItemText primary={`${game.players[0].number} - ${game.players[game.players.length - 1].number}`}
-                                                 secondary={t('game.players')}/>
-                                 </ListItem>}
-                {game.links && game.links.license && (
-                  <ListItem button
-                            component={Link}
-                            color="inherit"
-                            underline="none"
-                            target="_blank"
-                            href={game.links.license}>
-                    <ListItemIcon><LicenseIcon color="error" /></ListItemIcon>
-                    <ListItemText primary={t('game.license.primary')} secondary={t('game.license.secondary')} />
-                  </ListItem>
-                )}
-                {game.links && game.links.purchase && (
-                  <ListItem button
-                            component={Link}
-                            color="inherit"
-                            underline="none"
-                            target="_blank"
-                            href={game.links.purchase}>
-                    <ListItemIcon><PurchaseIcon style={{color: green[500]}}/></ListItemIcon>
-                    <ListItemText primary={t('game.purchase.primary')} secondary={t('game.purchase.secondary')} />
-                  </ListItem>
-                )}
-                {game.links && game.links.bgg && (
-                  <ListItem button
-                            component={Link}
-                            color="inherit"
-                            underline="none"
-                            target="_blank"
-                            href={game.links.bgg}>
-                    <ListItemIcon><BGGIcon/></ListItemIcon>
-                    <ListItemText>{t('game.bgg')}</ListItemText>
-                  </ListItem>
-                )}
-                {game.links && game.links.rules && (
-                  <ListItem button
-                            component={Link}
-                            color="inherit"
-                            underline="none"
-                            target="_blank"
-                            href={game.links.rules}>
-                    <ListItemIcon><RulesIcon/></ListItemIcon>
-                    <ListItemText primary={t('game.rules')} />
-                  </ListItem>
-                )}
-                {game.prototype && (
-                  <ListItem>
-                    <ListItemIcon><WarningIcon style={{color: blue[500]}}/></ListItemIcon>
-                    <ListItemText primary={t('prototype.prototype')} secondary={t('prototype.description')}/>
-                  </ListItem>
-                )}
-                {game.wip && (
-                  <ListItem>
-                    <ListItemIcon><WarningIcon className={classes.warning}/></ListItemIcon>
-                    <ListItemText primary={t('wip.wip')} secondary={t('wip.description')}/>
-                  </ListItem>
-                )}
-              </List>
-            </Paper>
-          </Container>
-        </Route>
-      </Switch>
-    </Suspense>
+    <Switch>
+      <Route path="/games/:slug/b18/map">
+        <B18Map/>
+      </Route>
+      <Route path="/games/:slug/b18/tiles/:color*">
+        <B18Tiles/>
+      </Route>
+      <Route path="/games/:slug/b18/tokens">
+        <B18Tokens/>
+      </Route>
+      <Route path="/games/:slug/background">
+        <Background/>
+      </Route>
+      <Route path="/games/:slug/cards/:type/:index">
+        <Card/>
+      </Route>
+      <Route path="/games/:slug/cards">
+        <Cards/>
+      </Route>
+      <Route path="/games/:slug/charters/:index">
+        <Charter/>
+      </Route>
+      <Route path="/games/:slug/charters">
+        <Charters/>
+      </Route>
+      <Route path="/games/:slug/map">
+        <Map/>
+      </Route>
+      <Route path="/games/:slug/market">
+        <Market/>
+      </Route>
+      <Route path="/games/:slug/par">
+        <Par/>
+      </Route>
+      <Route path="/games/:slug/revenue">
+        <Revenue/>
+      </Route>
+      <Route path="/games/:slug/tile-manifest">
+        <TileManifest/>
+      </Route>
+      <Route path="/games/:slug/tiles/:id">
+        <Tile/>
+      </Route>
+      <Route path="/games/:slug/tiles">
+        <Tiles/>
+      </Route>
+      <Route path="/games/:slug/tokens/:index">
+        <Token/>
+      </Route>
+      <Route path="/games/:slug/tokens">
+        <Tokens/>
+      </Route>
+      <Route>
+        <Container maxWidth="md">
+          <Paper elevation={5} className={classes.page}>
+            <Typography variant="h3">{game.info.title}</Typography>
+            {game.info.subtitle && <Typography variant="h5">{game.info.subtitle}</Typography>}
+            <Typography variant="h6">{t('game.by')} {game.info.designer}</Typography>
+            <List>
+              {game.players && <ListItem>
+                                  <ListItemIcon><PlayersIcon/></ListItemIcon>
+                                  <ListItemText primary={`${game.players[0].number} - ${game.players[game.players.length - 1].number}`}
+                                                secondary={t('game.players')}/>
+                                </ListItem>}
+              {game.links && game.links.license && (
+                <ListItem button
+                          component={Link}
+                          color="inherit"
+                          underline="none"
+                          target="_blank"
+                          href={game.links.license}>
+                  <ListItemIcon><LicenseIcon color="error" /></ListItemIcon>
+                  <ListItemText primary={t('game.license.primary')} secondary={t('game.license.secondary')} />
+                </ListItem>
+              )}
+              {game.links && game.links.purchase && (
+                <ListItem button
+                          component={Link}
+                          color="inherit"
+                          underline="none"
+                          target="_blank"
+                          href={game.links.purchase}>
+                  <ListItemIcon><PurchaseIcon style={{color: green[500]}}/></ListItemIcon>
+                  <ListItemText primary={t('game.purchase.primary')} secondary={t('game.purchase.secondary')} />
+                </ListItem>
+              )}
+              {game.links && game.links.bgg && (
+                <ListItem button
+                          component={Link}
+                          color="inherit"
+                          underline="none"
+                          target="_blank"
+                          href={game.links.bgg}>
+                  <ListItemIcon><BGGIcon/></ListItemIcon>
+                  <ListItemText>{t('game.bgg')}</ListItemText>
+                </ListItem>
+              )}
+              {game.links && game.links.rules && (
+                <ListItem button
+                          component={Link}
+                          color="inherit"
+                          underline="none"
+                          target="_blank"
+                          href={game.links.rules}>
+                  <ListItemIcon><RulesIcon/></ListItemIcon>
+                  <ListItemText primary={t('game.rules')} />
+                </ListItem>
+              )}
+              {game.prototype && (
+                <ListItem>
+                  <ListItemIcon><WarningIcon style={{color: blue[500]}}/></ListItemIcon>
+                  <ListItemText primary={t('prototype.prototype')} secondary={t('prototype.description')}/>
+                </ListItem>
+              )}
+              {game.wip && (
+                <ListItem>
+                  <ListItemIcon><WarningIcon className={classes.warning}/></ListItemIcon>
+                  <ListItemText primary={t('wip.wip')} secondary={t('wip.description')}/>
+                </ListItem>
+              )}
+            </List>
+          </Paper>
+        </Container>
+      </Route>
+    </Switch>
   )
 };
 
