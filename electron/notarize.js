2783c60e576ee3813248caf8b913fe9633ffa360
Christopher Giroir <kelsin@valefor.com>
2024-11-09 00:21:56 -0800

    fix: notarize

--- electron/notarize.js
+++ electron/notarize.js
@@ -1,4 +1,4 @@
-const { notarize } = require("@electron/notarize");
+import { notarize } from "@electron/notarize";
 
 async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
