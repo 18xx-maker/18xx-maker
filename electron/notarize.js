dd498e2765309eb09501addb76aa88b50d6cdf49
Christopher Giroir <kelsin@valefor.com>
2024-11-10 13:01:45 -0800

    chore: cleaning up notarize.js

--- electron/notarize.js
+++ electron/notarize.js
@@ -25,9 +25,6 @@ const notarizing = async (context) => {
     return;
   }
 
-  const appName = context.packager.appInfo.productFilename;
-  const appPath = `${appOutDir}/${appName}.app`;
-  const zipPath = `${appOutDir}/${appName}.zip`;
   const appleId = process.env.APPLE_ID;
   const appleTeamId = process.env.APPLE_TEAM_ID;
   const applePassword = process.env.APPLE_APP_SPECIFIC_PASSWORD;
@@ -36,15 +33,21 @@ const notarizing = async (context) => {
     return;
   }
 
-  console.log(`Creating ${zipPath} from ${appPath}`);
+  const appName = context.packager.appInfo.productFilename;
+  const appFile = `${appName}.app`;
+  const zipFile = `${appName}.zip`;
+  const appPath = `${appOutDir}/${appFile}`;
+  const zipPath = `${appOutDir}/${zipFile}`;
+
+  console.log(`Creating ${zipFile} from ${appFile}`);
   run(`ditto -c -k --keepParent "${appPath}" "${zipPath}"`);
 
-  console.log(`Submitting ${zipPath} for notarizing`);
+  console.log(`Submitting ${zipFile} for notarizing`);
   run(
     `xcrun notarytool submit "${zipPath}" --team-id "${appleTeamId}" --apple-id "${appleId}" --password "${applePassword}" --wait`,
   );
 
-  console.log(`Stapling ticket to ${appPath}`);
+  console.log(`Stapling ticket to ${appFile}`);
   run(`xcrun stapler staple "${appPath}"`);
 };
 
