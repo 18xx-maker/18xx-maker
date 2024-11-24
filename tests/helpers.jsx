72ea2ec7fcfda6eeebb52253f7a2e0b2fb26b970
Christopher Giroir <kelsin@valefor.com>
2024-11-24 13:03:33 -0800

    fix: apply fix from react-router team

--- tests/helpers.jsx
+++ tests/helpers.jsx
@@ -2,8 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
-import { createMemoryRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import { RouterProvider, createMemoryRouter } from "react-router";
 
 import { rootRoutes } from "@/routes";
 import { initialState, rootReducer } from "@/state";
