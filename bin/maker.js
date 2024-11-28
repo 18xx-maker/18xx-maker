12b7d375de6d87c418fc80b49b6924a2dc68a73c
Christopher Giroir <kelsin@valefor.com>
2024-11-28 02:13:55 -0800

    chore(cli): replace ajv with json-schema-library

--- bin/maker.js
+++ bin/maker.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { userInfo } from "node:os";
 
 import { program } from "commander";
--- src/cli/validate.js
+++ src/cli/validate.js
@@ -1,22 +1,31 @@
-#!/usr/bin/env node
 import fs from "node:fs";
 import path from "node:path";
 import util from "node:util";
 
-import Ajv from "ajv";
 import chalk from "chalk";
 import { globSync } from "glob";
+import jsl from "json-schema-library";
 
-import { all, chain, compose, identity, map } from "ramda";
+import {
+  addIndex,
+  all,
+  chain,
+  compose,
+  forEach,
+  identity,
+  map,
+  replace,
+} from "ramda";
 
 import { loadJSON, loadSchema } from "#cli/util";
 
-const ajv = new Ajv({ allErrors: true });
+const { Draft07 } = jsl;
 
 // Load Defs
 const tileDefs = loadSchema("tiles.defs.json");
 
 // Load Schemas
+const draftSchema = loadSchema("draft07.schema.json");
 const companiesSchema = loadSchema("companies.schema.json");
 const configSchema = loadSchema("config.schema.json");
 const gameSchema = loadSchema("game.schema.json");
@@ -24,18 +33,31 @@ const publishersSchema = loadSchema("publishers.schema.json");
 const themeSchema = loadSchema("theme.schema.json");
 const tilesSchema = loadSchema("tiles.schema.json");
 
-// Validate and load schemas
-ajv.addSchema([
-  tileDefs,
-  companiesSchema,
-  configSchema,
-  gameSchema,
-  publishersSchema,
-  themeSchema,
-  tilesSchema,
-]);
+const jsonSchema = new Draft07();
+jsonSchema.addRemoteSchema(tileDefs["$id"], tileDefs);
+
+const schemas = {};
+forEach(
+  (schema) => {
+    schemas[schema["$id"]] = schema;
+  },
+  [
+    draftSchema,
+    companiesSchema,
+    configSchema,
+    gameSchema,
+    publishersSchema,
+    themeSchema,
+    tilesSchema,
+  ],
+);
 
 const determineSchema = (json) => {
+  // Schemas themselves have a "$schema" field
+  if (json["$schema"]) {
+    return json["$schema"];
+  }
+
   // Games have an info object first thing
   if (json.info) {
     return gameSchema.$id;
@@ -67,13 +89,15 @@ const determineSchema = (json) => {
 
 let validate = (json, file) => {
   const id = determineSchema(json);
-  const valid = ajv.validate(id, json);
+
+  jsonSchema.setSchema(schemas[id]);
+  const validationErrors = jsonSchema.validate(json);
 
   return {
-    valid,
+    valid: validationErrors.length === 0,
     id,
     file,
-    validationErrors: ajv.errors,
+    validationErrors,
   };
 };
 
@@ -104,16 +128,49 @@ validate.file = (file) => {
 };
 
 const getShortSchemaName = (id) => {
+  let draft = id.match(/json-schema\.org/);
+  if (draft) {
+    return "schema";
+  }
   let results = id.match(/([a-z]+)\.schema\.json$/);
   return results ? results[1] : id;
 };
 
+const fixObject = replace(/`[^`]+` \(object\)/g, chalk.yellow("(object)"));
+const fixArray = replace(/`[^`]+` \(array\)/g, chalk.green("(array)"));
+const fixValueObject = replace(/`\{[^`]+\}`/g, chalk.yellow("(object)"));
+const fixValueArray = replace(/`\[[^`]+\]`/g, chalk.green("(array)"));
+const fixOther = replace(/`([^`]+)`/g, chalk.blueBright("$1"));
+const displayMessage = compose(
+  fixOther,
+  fixValueObject,
+  fixValueArray,
+  fixObject,
+  fixArray,
+);
+const displayPointer = (pointer) => {
+  return chalk.magentaBright(pointer);
+};
+const displayErrors = (errors = [], level = 0) => {
+  if (errors.length === 0) return;
+  addIndex(forEach)((error, index) => {
+    const padding = (chalk.white(".") + chalk.gray("...")).repeat(level);
+    process.stdout.write(
+      `${padding}${displayPointer(error.data.pointer)} ${displayMessage(error.message)}\n`,
+    );
+    displayErrors(error.data && error.data.errors, level + 1);
+
+    if (level === 0 && index === errors.length - 1) {
+      process.stdout.write("\n");
+    }
+  }, errors);
+};
 const displayResult = ({ valid, error, validationErrors, file, id }) => {
   const color = error ? chalk.red : valid ? chalk.green : chalk.yellow;
 
-  const result = (error ? "error" : valid ? "valid" : "invalid").padEnd(7, " ");
+  const result = (error ? "error" : valid ? "valid" : "invalid").padEnd(6, " ");
 
-  const name = getShortSchemaName(id).padEnd(7, " ");
+  const name = getShortSchemaName(id).padEnd(11, " ");
 
   const basename = path.basename(file);
   const dirname = path.relative(process.cwd(), path.dirname(file));
@@ -122,8 +179,8 @@ const displayResult = ({ valid, error, validationErrors, file, id }) => {
     `${color(result)} ${chalk.cyan(name)} ${basename} ${chalk.gray(dirname)}\n`,
   );
 
-  if (error || validationErrors) {
-    let output = util.inspect(error || validationErrors, {
+  if (error) {
+    let output = util.inspect(error, {
       showHidden: false,
       depth: null,
       colors: true,
@@ -132,6 +189,8 @@ const displayResult = ({ valid, error, validationErrors, file, id }) => {
     });
     process.stdout.write(`\n${output}\n\n`);
   }
+
+  displayErrors(validationErrors);
 
   return valid;
 };
