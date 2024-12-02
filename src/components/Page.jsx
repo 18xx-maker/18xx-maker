1b3bee7ade5b5525f49b4a8b61ab495525501cb6
Christopher Giroir <kelsin@valefor.com>
2024-12-01 23:08:42 -0800

    chore(ui): cleaning up game tiles

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,6 +1,8 @@
 const Page = ({ title, component, current, total }) => (
-  <div className="cutlines__page">
-    {title} - {component} - page {current} of {total}
+  <div className="hidden print:block h-12 absolute top-0 w-full flex flex-row justify-center content-center">
+    <p className="text-center">
+      {title} - {component} - page {current} of {total}
+    </p>
   </div>
 );
 
