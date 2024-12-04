5e77dcf6c3233aaec8fe1044b28eb5549b4d74f6
Christopher Giroir <kelsin@valefor.com>
2024-12-04 08:15:56 -0800

    chore(ui): convert config from MUI to shadcn

--- src/components/Viewport.jsx
+++ src/components/Viewport.jsx
@@ -1,12 +1,17 @@
 import Toolbar from "@/components/Toolbar";
+import Config from "@/components/config/Config";
+
+import { useBooleanParam } from "@/util/query";
 
 const Viewport = ({ children }) => {
+  const [config] = useBooleanParam("config");
   return (
     <div
       id="viewport"
-      className="editor-checkered print:bg-none print:bg-white"
+      className="editor-checkered print:bg-none print:bg-white select-none overscroll-none"
     >
       <Toolbar />
+      {config && <Config />}
       {children}
     </div>
   );
--- src/components/config/UnitInput.jsx
+++ src/components/config/UnitInput.jsx
@@ -1,29 +1,17 @@
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 
 import { keys, map } from "ramda";
 
-import Box from "@mui/material/Box";
-import MUIInput from "@mui/material/FilledInput";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
-import makeStyles from "@mui/styles/makeStyles";
-
-const useStyles = makeStyles((theme) => ({
-  configItem: {
-    minWidth: 300,
-    margin: theme.spacing(3, 0, 0, 0),
-    flexDirection: "row",
-  },
-  configInput: {
-    width: 200,
-  },
-  configUnits: {
-    width: 100,
-    marginLeft: theme.spacing(1),
-  },
-}));
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const allUnits = {
   inches: 100.0,
@@ -32,7 +20,6 @@ const allUnits = {
 
 // Component to help input units
 const UnitInput = ({ name, value, label, onChange, errorValidation }) => {
-  const classes = useStyles();
   let [error, setError] = useState(false);
   let [units, setUnits] = useState("inches");
   let [internalValue, setInternalValue] = useState(value / allUnits[units]);
@@ -61,44 +48,44 @@ const UnitInput = ({ name, value, label, onChange, errorValidation }) => {
     onChange(numberValue * allUnits[units]);
   };
 
-  let unitsHandler = (event) => {
-    setUnits(event.target.value);
-    setInternalValue(value / allUnits[event.target.value]);
+  let unitsHandler = (newValue) => {
+    setUnits(newValue);
+    setInternalValue(value / allUnits[newValue]);
   };
 
+  const className = clsx({ "border-error": isError });
+  const numberClassName = clsx(className, "w-24");
+
   return (
-    <Box className={classes.configItem}>
-      <FormControl variant="filled" error={isError}>
-        <InputLabel id={`${name}-label`}>{label}</InputLabel>
-        <MUIInput
+    <div className="mt-4">
+      <Label className="w-min text-lg" htmlFor={name}>
+        {label}
+      </Label>
+      <div className="flex flex-row gap-2 my-1 justify-start items-center">
+        <Input
           id={name}
           name={name}
-          className={classes.configInput}
-          variant="filled"
-          inputProps={{ type: "input" }}
           value={internalValue}
           onChange={handler}
+          className={numberClassName}
         />
-      </FormControl>
-      <FormControl variant="filled">
-        <Select
-          id={`${name}-units`}
-          labelId={`${name}-label`}
-          className={classes.configUnits}
-          value={units}
-          onChange={unitsHandler}
-        >
-          {map(
-            (key) => (
-              <MenuItem key={key} value={key}>
-                {key}
-              </MenuItem>
-            ),
-            keys(allUnits),
-          )}
+        <Select onValueChange={unitsHandler} value={units}>
+          <SelectTrigger className={className}>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            {map(
+              (key) => (
+                <SelectItem key={key} value={key}>
+                  {key}
+                </SelectItem>
+              ),
+              keys(allUnits),
+            )}
+          </SelectContent>
         </Select>
-      </FormControl>
-    </Box>
+      </div>
+    </div>
   );
 };
 
